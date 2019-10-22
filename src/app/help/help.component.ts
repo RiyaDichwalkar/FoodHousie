3c3998c9ad9ab76ef16782286999e91f6d7057c9
Riya Dichwalkar <riyadichwalkar01@gmail.com>
2019-10-22 15:26:44 +0530

    Help and Contact

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AuthService } from "../auth/auth.service";
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
-
-  constructor() { }
-
+  feedback:string;
+  user:any;
+  constructor(private db: AngularFirestore,public authService: AuthService,private router:Router ) { }
+   role:string;
   ngOnInit() {
-  }
 
+  }
+  onSubmitHelp(){
+    
+   this.user =JSON.parse(localStorage.getItem("user"));
+   if(JSON.parse(localStorage.getItem("roles")).isChef){
+     this.role="chef";
+   }else{
+    this.role="customer";
+   }
+    var id=this.db.createId()
+    this.db.collection('issue').doc(id)
+    .set({
+      issueid:id,
+      userid:this.user.uid,
+      username:this.user.fullname,
+      email:this.user.email,
+      role:this.role,
+      issue:this.feedback,
+      acknowledge:false
+    });
+    window.alert("your feedback is recieved");
+    this.router.navigate(["dashboard"]);
+  }
 }
